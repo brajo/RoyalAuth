aa0b4b858e8a2779a371bfe3431faa1087643226
brajo <jordan@brajo.co.uk>
2013-11-11 19:44:18 +0000

    Update CmdLogin.java

--- src/main/java/org/royaldev/royalauth/commands/CmdLogin.java
+++ src/main/java/org/royaldev/royalauth/commands/CmdLogin.java
@@ -10,7 +10,6 @@ import org.royaldev.royalauth.Config;
 import org.royaldev.royalauth.Hasher;
 import org.royaldev.royalauth.RUtils;
 import org.royaldev.royalauth.RoyalAuth;
-import org.royaldev.royalauth.ChatManager;
 
 import java.security.NoSuchAlgorithmException;
 
@@ -62,7 +61,6 @@ public class CmdLogin implements CommandExecutor {
                 ap.login();
                 plugin.getLogger().info(p.getName() + " has logged in.");
                 cs.sendMessage(ChatColor.BLUE + "You have been logged in successfully.");
-                plugin.getChatManager().enableChat((Player)cs);
             } else {
                 plugin.getLogger().warning(p.getName() + " tried to log in with an incorrect password!");
                 cs.sendMessage(ChatColor.RED + "That password was incorrect. Please try again.");
